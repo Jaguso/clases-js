13ec2d4aeba4f4243deda9ff820f03573a321067
Jaguso <jagutzo@gmail.com>
2022-11-10 18:09:36 -0600

    first commit

---
+++ clases1.js
@@ -0,0 +1,41 @@
+console.log('Hola mundo');
+
+// comentarios
+
+// TIPOS DE VARIABLE
+
+// Definición de una variable
+//ejemplos:
+var estatura = 175;
+var numero = 3;
+
+console.log(numero)
+console.log(estatura)
+
+
+// 1) String (cadena)
+var nombreUsuario = 'Roberto Cantoral';  // <-- string o cadena
+// es equivalente a "Roberto Cantoral"
+
+// 2) Number (numero)
+var edad = 30; // <-- number o número
+
+
+// 3) Array (arreglo)
+// El siguiente es un tipo de dato llamado arreglo o array
+var listaDeNombres = ['Juan', 'Pedro', 'Judas']
+
+var arrayPrueba = [42, 'Roberto', 12, [1,2,3,4]]
+
+/////////////////////////////////////
+
+// Cómo preguntar a una variable qué tipo de dato es:
+
+var edad = 30;
+console.log(typeof(edad))
+
+var nombre = 'Juan'
+console.log(typeof(nombre))
+
+var arregloEdades = [23,25,50,12]
+console.log(typeof(arregloEdades))
